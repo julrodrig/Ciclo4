121df22c55beaa513ecd99cd749d0bc5f4dfd27c
Julian Rodriguez <julrodrig@gmail.com>
2022-10-18 23:21:59 -0500

    APP.REACT-v1 + VITE+REACT-v2

---
+++ app-react-v2/src/componets/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div> <h1>TITULO DE PAGINA </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ calculadora.js
@@ -0,0 +1,11 @@
+
+
+const calculadora = (a,b) => {
+  return  `La suma de ${a} + ${b} es igual a ${a+b}` ;
+
+};
+
+console.log (calculadora(3,5));
+
+
+
