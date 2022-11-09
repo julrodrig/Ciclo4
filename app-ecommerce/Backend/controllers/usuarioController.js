0f61d361fffc23e74efccc899393b887227e9b9c
Julian Rodriguez <julrodrig@gmail.com>
2022-11-08 22:59:00 -0500

    Update + Express + mongoose + Test MVC

---
+++ app-ecommerce/Backend/controllers/usuarioController.js
@@ -0,0 +1,15 @@
+const prueba = (req, res) => {
+
+  res.send({
+
+      msg: "En esta ruta gestionaremos todas las peticiones correspondiente al modelo de Usarios"
+
+  })
+
+};
+
+export {
+
+  prueba
+
+};
--- app-ecommerce/Backend/index.js
+++ app-ecommerce/Backend/index.js
@@ -1,18 +1,46 @@
+// https://expressjs.com/es/
 import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import conectarDB from './config/db.js';
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import productoRoutes from './routes/productoRoutes.js'; 
+import ventaRoutes from './routes/ventaRoutes.js';
 
-const PORT = process.env.PORT || 4000;  // Varlible de Entorno Puerto de la app
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+// Se le agrega toda la funcionalidad del servidor de express
 const app = express();
+app.use(express.json());
 
+conectarDB();
+// middlewares
+// Se utiliza para realizar la comunicacion entre el servidor del frontend y el backend
+const dominiosPermitidos = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (dominiosPermitidos.indexOf(origin) !== -1) {
+            // El origen del Request esta permitido
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+};
+app.use(cors(corsOptions));
 
-// Middleware
-// req: Información que nos llega
-// res: respuesta nuestra
+// Gestion Modelo de Usuario
+app.use('/api/usuarios', usuarioRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo ExpresJS te saluda G70 UCaldas MisionTic')
-  console.log("Llego una peticion -- nodemon");
-});
+// Gestion Modelo de Producto
+app.use('/api/productos', productoRoutes);
+
+// Gestion Modelo de Ventas
+app.use('/api/ventas', ventaRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor inicializado en el puerto # ${PORT}`)
+    console.log(`Servidor funcionando en el puerto ${PORT} `);
 });
